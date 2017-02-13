fedf6a3b53c7f216a5000bafb1771b62f3aa8fb3
Sierra 4 <sierra4@Sierras-iMac.local>
2017-02-13 11:37:58 +0530

    initial commit

--- ShapeAnimation/ViewController.swift
+++ ShapeAnimation/ViewController.swift
@@ -10,16 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var viewSquare: UIView!
+    
+    var location = CGPoint(x: 0, y: 0)
+    
+    var doubleTap : Bool = false
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch: UITouch! = touches.first! as UITouch
+        location = touch.location(in: self.view)
+        viewSquare.center = location
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch: UITouch! = touches.first! as UITouch
+        location = touch.location(in: self.view)
+        viewSquare.center = location
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //UIView.animate(withDuration: 5, animations: m)
+        viewSquare.center = CGPoint(x: 160,y: 330)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func btnClick(_ sender: Any) {
+        
+        
+        
+        if (doubleTap) {
+            viewSquare.layer.cornerRadius = 0
+            doubleTap = false
+        } else {
+            viewSquare.layer.cornerRadius = viewSquare.frame.height/2
+            doubleTap = true
+        }
+        /*
+        var doubleTap : Bool! = false
+        if doubleTap == true {
+            viewSquare.layer.cornerRadius = viewSquare.frame.height
+            //doubleTap = false
+        } else {
+            viewSquare.layer.cornerRadius = viewSquare.frame.height/2
+            //doubleTap = true
+        }*/
     }
-
-
 }
 
